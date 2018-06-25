71f10af77bf0c73f4294994831e7758e4083f4cb
root <const.subject@gmail.com>
2018-06-25 15:39:51 -0400

    Change version

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const init = require('./src/init');
  * @type {{version: string, buildVersion: string, config: string, genesis: string, basePath: string, htmlPath: string, dappsPath: string, netVersion: string, blockchainFile: string, debug: string[]}}
  */
 let defaultConfig = {
-    version: '1.1.4',
+    version: '1.1.5',
     buildVersion: 'rc',
     config: 'config.json',
     genesis: 'genesis.json',
